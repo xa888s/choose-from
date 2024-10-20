0007724e676af1401ee3e0c822c7c91b4d20268e
xa888s <xa888s@cs.sfu.ca>
2024-10-20 11:05:44 -0700

    Reworked API to be more intuitive

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//! Choose a K-selection of values from N choices, where N and K are set at compile time.
+//! Choose a K-selection of values from N choices, where N and K are set at compile time (or not).
 //!
 //! # Why is this useful?
 //!
@@ -26,7 +26,10 @@
 //!     }
 //!     
 //!     // where chooser is some external function that chooses from the provided suits
-//!     pub fn choose_suit(&self, chooser: impl FnOnce([Suit; N]) -> Suit) {
+//!     pub fn choose_suit<C>(&self, chooser: C)
+//!     where
+//!         C: FnOnce([Suit; N]) -> Suit
+//!     {
 //!         // have user choose some suit
 //!         let suit = chooser(self.0);
 //!
@@ -56,7 +59,10 @@
 //! #     pub fn with_suits(suits: [Suit; N]) -> Suits<N> {
 //! #         Suits(suits)
 //! #     }
-//! #     pub fn choose_suit(&self, chooser: impl FnOnce([Suit; N]) -> Suit) {
+//! #     pub fn choose_suit<C>(&self, chooser: C)
+//! #     where
+//! #         C: FnOnce([Suit; N]) -> Suit
+//! #     {
 //! #         // have user choose some suit
 //! #         let suit = chooser(self.0);
 //! #
@@ -70,10 +76,9 @@
 //! // include spades
 //! suits.choose_suit(|_| Suit::Spades);
 //! ```
-//! This is where we can use a Choose struct to force the user to take one of our
+//! This is where we can use the functions in this library to force the user to take one of our
 //! provided choices
 //! ```
-//! # use choose_from::{Choice, Choose};
 //! # #[derive(Clone, Copy)]
 //! # enum Suit {
 //! #     Clubs,
@@ -85,17 +90,20 @@
 //! # #[derive(Clone, Copy)]
 //! # struct Suits<const N: usize>([Suit; N]);
 //! #
+//! use choose_from::{choose_from_fixed, Choice};
+//!
 //! impl<const N: usize> Suits<N> {
 //! #   pub fn with_suits(suits: [Suit; N]) -> Suits<N> {
 //! #         Suits(suits)
 //! #   }
 //!     // ...
 //!     // where chooser is some external function that chooses from the provided suits
-//!     pub fn choose_suit(&self, chooser: impl FnOnce([Choice<'_, Suit>; N]) -> [Choice<'_, Suit>; 1]) {
-//!         // type inference :D (type would be: Choose<1, Suit, N>)
-//!         let choices = Choose::with_choices(self.0);
+//!     pub fn choose_suit<C>(&self, chooser: C)
+//!     where
+//!         C: FnOnce([Choice<'_, Suit>; N]) -> [Choice<'_, Suit>; 1]
+//!     {
 //!         // have user choose some suit (this suit is guaranteed to be from our choices)
-//!         let [suit]: [Suit; 1] = choices.choose_with(chooser);
+//!         let [suit]: [Suit; 1] = choose_from_fixed(self.0).with(chooser);
 //!
 //!         // do stuff with suit
 //!         // ...
@@ -103,117 +111,138 @@
 //!     // ...
 //! }
 //! ```
+//! ## Alternative?
+//!
+//! If you thought about it for a bit, you may realize that you can just use an enum
+//! over "choosable" values, and then providing a mapping from that enum to our original
+//! values:
+//! ```
+//! # #[derive(Clone, Copy)]
+//! # enum Suit {
+//! #     Clubs,
+//! #     Diamonds,
+//! #     Hearts,
+//! #     Spades,
+//! # }
+//! #
+//! # #[derive(Clone, Copy)]
+//! # struct Suits<const N: usize>([Suit; N]);
+//! #
+//! pub enum ChoosableSuit {
+//!     Clubs,
+//!     Diamonds,
+//! }
+//!
+//! impl ChoosableSuit {
+//!     pub fn to_suit(self) -> Suit {
+//!         match self {
+//!             ChoosableSuit::Clubs => Suit::Clubs,
+//!             ChoosableSuit::Diamonds => Suit::Diamonds,
+//!         }
+//!     }
+//! }
+//!
+//! impl<const N: usize> Suits<N> {
+//! #   pub fn with_suits(suits: [Suit; N]) -> Suits<N> {
+//! #         Suits(suits)
+//! #   }
+//!     // ...
+//!     // where chooser is some external function that chooses from the provided suits
+//!     pub fn choose_suit<C>(&self, chooser: C)
+//!     where
+//!         C: FnOnce([ChoosableSuit; 2]) -> ChoosableSuit
+//!     {
+//!         // have user choose some suit (let's imagine these ChoosableSuits are from our choices)
+//!         let suit: Suit = chooser([ChoosableSuit::Clubs, ChoosableSuit::Diamonds]).to_suit();
+//!
+//!         // do stuff with suit
+//!         // ...
+//!     }
+//!     // ...
+//! }
+//! ```
+//! This works! But this only works for returning a single value (plus it is kind of annoying to write a bunch of
+//! boilerplate enums everytime you want to choose between some values). When we try to return multiple (non-duplicate) values
+//! (as an array, tuple, Vec, etc.), we run into the same problem as earlier, where we can't stop a user from providing
+//! two or more duplicate choices (this is an example of choices *with* replacement, when we want choices *without* replacement).
+//!
+//! ## Concrete use case
+//!
 //! Let's imagine `chooser` to be some GUI selector. This allows us to abstract away the
 //! logic of actually getting a choice from an application user to the user of our library.
 //! Which means that multiple implementations of `chooser` can use our library (web app, CLI, desktop, etc.).
+//!
+//! # How do they work?
+//!
+//! Values are assured to be from the selection through two ways.
+//! First the only constructor for [Choice] is private
+//! ```compile_fail
+//! use choose_from::choose_from_fixed;
+//!
+//! // we cannot access the private constructor. And it requires a reference
+//! // to a Guard that we cannot construct
+//! let one = Choice::with_guard(1, unreachable!());
+//! ```
+//! So we know choices cannot be created out of thin air, but what about the
+//! owned [Choice]s provided to us through [`with`](crate::Selector::with) (or similar methods)?
+//! If we moved them out of the closure (since we have ownership), and then used them as choices
+//! for a new [choose_from] with the same type, then we could return values that aren't from the
+//! available choices! If we try to do that:
+//! ```compile_fail
+//! use choose_from::choose_from_fixed;
+//!
+//! let mut smuggler = Vec::new();
+//! choose_from(vec![1, 2, 3, 4]).any_with(|choices| {
+//!     // try to move last three values out of the closure
+//!     smuggle.extend(choices.drain(1..));
+//!     choices
+//! });
+//!
+//! // use the smuggled value later to do nefarious stuff
+//! // if this was possible weird_values wouldn't be from our
+//! // provided choices
+//! let weird_values = choose_from(vec![]).any_with(|_| smuggler);
+//! ```
+//! This fails to compile. Remember the Guard we mentioned earlier? All choices have a
+//! lifetime specifier. They don't actually hold any value, but they act as if they hold
+//! a reference to a Guard. This stops a [Choice] from living longer than the call to
+//! [with](crate::SelectorArr) (and similar methods), since the reference for each Guard
+//! only lives as long as the body of the method (since [Choice] "holds" a reference to the guard,
+//! it cannot live longer than it). Both of these steps combine to ensure that the `chooser`
+//! function *MUST* select value(s) from the provided ones.
+//!
+//! If you are interested in learning more try reading the code, it is quite simple.
 
-#[derive(Debug)]
-struct Guard;
+mod arr;
+mod choice;
+mod vec;
 
-/// A specific choice, yielded by [`Choose::with_choices`]
-#[derive(Debug)]
-pub struct Choice<'guard, T> {
-    value: T,
-    _guard: &'guard Guard,
+pub use arr::SelectorArr;
+pub use choice::Choice;
+use choice::Guard;
+pub use vec::Selector;
+
+/// Wraps our choices and allows us to choose from them
+pub fn choose_from<T>(choices: Vec<T>) -> Selector<T> {
+    Selector::with_choices(choices)
 }
 
-impl<'guard, T> Choice<'guard, T> {
-    fn with_guard(value: T, guard: &'guard Guard) -> Choice<'guard, T> {
-        Choice {
-            value,
-            _guard: guard,
-        }
-    }
-
-    /// Allows you to inspect the value of the choice
-    pub fn value(&self) -> &T {
-        &self.value
-    }
-
-    fn into_inner(self) -> T {
-        self.value
-    }
-}
-
-/// Container that guarantees a selection of its values, where N is
-/// the possible choices, and K is the size of the selection
-// Read as: Choose K Ts from N (Ts)
-#[derive(Debug, Hash, PartialEq, Eq)]
-pub struct Choose<const K: usize, T, const N: usize> {
-    choices: [T; N],
-}
-
-impl<const K: usize, T, const N: usize> Choose<K, T, N> {
-    /// Creates a Choose struct with provided choices
-    /// ```
-    /// # use choose_from::Choose;
-    /// // here we are creating a Choose to select 2 elements from 3
-    /// let choices: Choose<2, _, 3> = Choose::with_choices([1, 2, 3]);
-    ///
-    /// // choose 2 from 3
-    /// // ...
-    /// ```
-    pub fn with_choices(choices: [T; N]) -> Choose<K, T, N> {
-        const {
-            assert!(
-                N >= K,
-                "Selection must be smaller than or equal to the number of choices"
-            );
-        }
-        Choose { choices }
-    }
-
-    /// The main method of this type. The closure `chooser` is used to take choice(s) from our provided
-    /// choice(s) by returning a K-selection of it. The values of these choices are then
-    /// returned by the function.
-    /// ```
-    /// # use choose_from::Choose;
-    /// // types are inferred :D
-    /// let choices = Choose::with_choices(["Hi", "how", "are ya?"]);
-    ///
-    /// let chosen = choices.choose_with(|[first, second, third]| {
-    ///     // the provided choices allow inspection of the values
-    ///     let first_val: &&str = first.value();
-    ///     assert_eq!(*first_val, "Hi");
-    ///     
-    ///     // this is our selection
-    ///     [first, third]
-    /// });
-    ///
-    /// assert_eq!(chosen, ["Hi", "are ya?"]);
-    /// ```
-    // we pass our possible choices to the function wrapped in Choice, which only allows
-    // inspection of the value, and we must return an array of size K back full
-    // of our choices. The values returned are GUARANTEED to only come from our original
-    // choices thanks to the Choice struct and guard
-    pub fn choose_with(
-        self,
-        chooser: impl FnOnce([Choice<'_, T>; N]) -> [Choice<'_, T>; K],
-    ) -> [T; K] {
-        // here we use a guard to prevent the caller from "smuggling" a value out of the closure.
-        // This ensures that Choice values built from our given choices are only
-        // available within the closure (they can't escape), since Choice has no
-        // publicly accessible constructor.
-        //
-        // For example, this should fail to compile:
-        //
-        // let choices = Choose::with_choices([1, 2, 3, 4]);
-        // let mut extras = Vec::new();
-        // let chosen = choices.choose_with(|[one, two, three, _]| {
-        //     extras.push(three);
-        //     [one, two]
-        // });
-        //
-        // because extras is being used to "smuggle" the third choice out, which would be possible
-        // if we didn't have a guard since we are passing the closure an array of owned Choices
-        //
-        let _guard = Guard;
-        let choices = self.choices.map(|t| Choice::with_guard(t, &_guard));
-
-        chooser(choices).map(Choice::into_inner)
-        // _guard is dropped when function returns, which means that no one
-        // has any Choice values anymore
-    }
+/// Wraps our fixed number of choices and allows us to choose from them
+/// ```
+/// use choose_from::choose_from_fixed;
+/// let chosen = choose_from_fixed(["Hi", "how", "are ya?"]).with(|[first, second, third]| {
+///     // the provided choices allow inspection of the values
+///     let first_val: &&str = first.value();
+///     assert_eq!(*first_val, "Hi");
+///     
+///     // this is our selection
+///     [first, third]
+/// });
+///
+/// assert_eq!(chosen, ["Hi", "are ya?"]);
+/// ```
+pub fn choose_from_fixed<const N: usize, T>(choices: [T; N]) -> SelectorArr<N, T> {
+    SelectorArr::with_choices(choices)
 }
 
 #[cfg(test)]
@@ -221,10 +250,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn choose_two_i32s_from_four() {
-        let choices = Choose::with_choices([1, 2, 3, 4]);
-        let chosen = choices.choose_with(|[one, two, _, _]| [one, two]);
+    fn choose_two_i32s_from_fixed_four() {
+        let choices = [1, 2, 3, 4];
+        let chosen = choose_from_fixed(choices).with(|[one, two, _, _]| [one, two]);
 
         assert_eq!(chosen, [1, 2]);
     }
+
+    // TODO: write more tests
 }
---
+++ src/vec.rs
@@ -0,0 +1,91 @@
+use crate::{choice, Choice, Guard};
+
+/// Wraps a variable amount of choices and provides methods that guarantee selection from those choices.
+#[derive(Debug, Hash, PartialEq, Eq)]
+pub struct Selector<T> {
+    choices: Vec<T>,
+}
+
+impl<T> Selector<T> {
+    pub(crate) fn with_choices(choices: Vec<T>) -> Selector<T> {
+        Selector { choices }
+    }
+
+    /// The function `chooser` is used to choose from our provided
+    /// choices by returning a K-selection of it. The values of these choices are then
+    /// returned by the function.
+    /// ```
+    /// use choose_from::choose_from;
+    /// let choices = vec!["Hi", "how", "are ya?"];
+    ///
+    /// let chosen = choose_from(choices).with(|mut choices| {
+    ///     // the provided choices allow inspection of the values
+    ///     let third = choices.pop().unwrap();
+    ///     assert_eq!(*third.value(), "are ya?");
+    ///     
+    ///     // ignore 2nd
+    ///     choices.pop();
+    ///
+    ///     let first = choices.pop().unwrap();
+    ///     
+    ///     // this is our selection
+    ///     [first, third]
+    /// });
+    ///
+    /// assert_eq!(chosen, ["Hi", "are ya?"]);
+    /// ```
+    // we pass our possible choices to the function wrapped in Choice, which only allows
+    // inspection of the value, and it must return an array of size K back full
+    // of our choices. The values returned are GUARANTEED to only come from our original
+    // choices thanks to the Choice struct and guard
+    pub fn with<const K: usize, C>(self, chooser: C) -> [T; K]
+    where
+        C: FnOnce(Vec<Choice<'_, T>>) -> [Choice<'_, T>; K],
+    {
+        // here we use a guard to prevent the caller from "smuggling" a value out of the closure.
+        // This ensures that Choice values built from our given choices are only
+        // available within the closure (they can't escape), since Choice has no
+        // publicly accessible constructor.
+        let _guard = Guard;
+
+        let choices = self.into_choices(&_guard);
+
+        chooser(choices).map(Choice::into_inner)
+        // _guard is dropped when function returns, which means that no one
+        // has any Choice values anymore
+    }
+
+    /// Like [with](Selector::with), but for returning any number of chosen values. Use this when
+    /// you want to ensure some values come from the choices, but the amount of chosen values returned
+    /// doesn't matter.
+    /// ```
+    /// use choose_from::choose_from;
+    ///
+    /// let choices = vec!["Hi", "how", "are ya?"];
+    ///
+    /// let chosen = choose_from(choices).any_with(|choices| {
+    ///     choices.into_iter().step_by(2).collect()
+    /// });
+    ///
+    /// assert_eq!(chosen, ["Hi", "are ya?"]);
+    /// ```
+    pub fn any_with<C>(self, chooser: C) -> Vec<T>
+    where
+        C: FnOnce(Vec<Choice<'_, T>>) -> Vec<Choice<'_, T>>,
+    {
+        let _guard = Guard;
+        let choices = self.into_choices(&_guard);
+
+        choice::to_values(chooser(choices))
+    }
+
+    fn into_choices(self, _guard: &'_ Guard) -> Vec<Choice<'_, T>> {
+        // TODO: check optimization. This is probably optimized well since
+        // choices should have the same size and alignment as T so the collection
+        // may not need to reallocate
+        self.choices
+            .into_iter()
+            .map(|t| Choice::with_guard(t, _guard))
+            .collect()
+    }
+}
